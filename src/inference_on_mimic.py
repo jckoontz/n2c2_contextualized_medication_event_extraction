534bb79a6edfdc20fc07c7c0535b8b02c0768eb1
Jordan Koontz <chuckkoontz@chucks-macbook-pro.home>
2022-04-13 15:11:32 +0200

    inference on mimic added, model.py and predict_tags.py refactor

--- src/inference_on_mimic.py
+++ src/inference_on_mimic.py
@@ -2,8 +2,6 @@ import sys
 import os
 import argparse
 import yaml
-import glob
-import os
 from tqdm import tqdm
 import spacy
 import pytorch_lightning as pl
@@ -11,7 +9,8 @@ import torch
 from torch.utils.data import DataLoader, TensorDataset
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from transformers import AutoTokenizer
-
+import pandas as pd
+import random
 
 from model import NER_CRF
 
@@ -24,14 +23,16 @@ def main(args=None):
 
     config = load_config(args.config)
 
-    nlp = spacy.load("en_core_web_sm")
+    nlp = spacy.load('en_core_web_sm', disable=[
+                     'tagger', 'parser', 'ner', 'lemmatizer'])
+    nlp.add_pipe("sentencizer")
 
-    evaluation_documents = get_evaluation_documents(args.eval_data)
+    documents = process_data(args.note_events, nlp)
 
     ner_model = NER_CRF.load_from_checkpoint(config['checkpoint'],
                                              hparams_file=config['hparams'])
 
-    process_sentences(evaluation_documents, nlp, ner_model, args.outpath)
+    process_sentences(documents, ner_model, args.outpath)
 
 
 def load_config(config_path: str) -> dict:
@@ -43,46 +44,29 @@ def load_config(config_path: str) -> dict:
         return config
 
 
-def get_evaluation_documents(path: str) -> list:
-    return [file for file in glob.glob(os.path.join(path, '*.txt'))]
-
-
-def get_offset_to_sentences_dict(text, nlp_model) -> dict:
-    doc = nlp_model(text)
-    offset_to_sentences = {}
-    for sent in doc.sents:
-        for word in sent:
-            offset_to_sentences[word.idx] = word.text
-
-    sentences = []
-    tmp = []
-    offset_tmp = {}
-    offset_sentence_map = {}
-    i = 0
-    j = 0
-    max_len = 0
-    for offset, token in offset_to_sentences.items():
-        if token == ' ' or j >= 50:
-            if tmp:
-                sentences.append(tmp)
-                if len(tmp) > max_len:
-                    max_len = len(tmp)
-                offset_sentence_map[i] = {
-                    'sentence': tmp, 'offsets': offset_tmp}
-                i += 1
-                j = 0
-                tmp = []
-                offset_tmp = {}
-        else:
-            j += 1
-            tmp.append(str(token))
-            offset_tmp[offset] = str(token)
-
-    return offset_sentence_map, sentences
+def process_data(path: str, nlp):
+    notes_df = pd.read_csv(path)
+    sample = round(len(notes_df['SUBJECT_ID'].unique()) * 0.0001)
+    patient_ids = random.sample(
+        notes_df['SUBJECT_ID'].unique().tolist(), sample)
+    texts = []
+    notes_df_filtered = notes_df[notes_df['SUBJECT_ID'].isin(
+        patient_ids)].TEXT.tolist()
+    for note in notes_df_filtered:
+        doc = nlp(note)
+        tmp = []
+        for sent in doc.sents:
+            tokens = sent.text.split()
+            if tokens:
+                tmp.append(tokens)
+            else:
+                continue
+        texts.append(tmp)
+    return texts
 
 
 def prepare_batch_for_inference(sentences: list):
-    MAX_LEN = 128
+    MAX_LEN = 300
     tokenizer = AutoTokenizer.from_pretrained(
         "emilyalsentzer/Bio_ClinicalBERT", do_lowercase=False)
     tokenized_texts = bert_tokenize_for_inference(sentences, tokenizer)
@@ -92,12 +76,13 @@ def prepare_batch_for_inference(sentences: list):
     input_ids_tensors = torch.tensor(input_ids)
     attention_masks_tensors = torch.tensor(attention_masks)
     dataset = TensorDataset(input_ids_tensors, attention_masks_tensors)
-    dataloader = DataLoader(dataset, batch_size=2)
+    dataloader = DataLoader(dataset, batch_size=32)
     return dataloader, tokenized_texts
 
 
 def bert_tokenize_for_inference(sentences: list, tokenizer) -> tuple:
     tokenized_texts = []
+    print('here')
     for sentence in tqdm(sentences):
         temp_token = []
         # Add [CLS] at the front
@@ -156,33 +141,18 @@ def get_mentions(output: tuple) -> list:
     return mentions
 
 
-def find_sub_list(sl, l):
-    sll = len(sl)
-    for ind in (i for i, e in enumerate(l) if e == sl[0]):
-        if l[ind:ind+sll] == sl:
-            return ind, ind+sll-1
-
-
-def write_results(tokenized_texts, preds, offset_sentence_map, out_path: str):
+def write_results(tokenized_texts, preds, out_path: str):
     j = 2
     for i in tqdm(range(len(tokenized_texts))):
-        assert len(tokenized_texts) == len(preds)
-        output = reconstruct_sentences(tokenized_texts[i][:128], preds[i])
+        output = reconstruct_sentences(tokenized_texts[i][:300], preds[i])
         mentions = get_mentions(output)
-        offsets_tuples = list(offset_sentence_map[i]['offsets'].items())
-        tokens = [tup[1].lower() for tup in offsets_tuples]
         if not mentions:
-            #print('no mentions found')
             continue
         else:
             for mention in mentions:
                 try:
-                    start, end = find_sub_list(mention, tokens)
-                    start = offsets_tuples[start][0]
-                    end = offsets_tuples[end][0] + len(mention[-1])
-                    #offset = (start, end)
                     mention_ = ' '.join(entity for entity in mention)
-                    entry = f'T{j}\tDisposition {start} {end}\t{mention_}\n'
+                    entry = f'T{j}\Medication {mention_}\n'
                     with open(out_path, 'a+') as file:
                         file.write(entry)
                         j += 1
@@ -190,24 +160,12 @@ def write_results(tokenized_texts, preds, offset_sentence_map, out_path: str):
                     continue
 
 
-def process_sentences(evaluation_documents: list, nlp_model, ner_model, outpath: str):
-    for document in tqdm(evaluation_documents):
-        with open(document) as f:
-            text = f.read()
-        tail = os.path.split(document)[1].split('.')[0]
-        file_out = f'{tail}.ann'
-        file_out2 = f'{tail}.txt'
-        out_path = os.path.join(outpath, file_out)
-        out_path2 = os.path.join(outpath, file_out2)
-        offset_sentence_map, sentences = get_offset_to_sentences_dict(
-            text, nlp_model)
-        for sentence in sentences:
-            with open(out_path2, 'a') as f:
-                f.write(sentence)
+def process_sentences(documents: list, ner_model, outpath: str):
+    for document in tqdm(documents):
         batch_for_inference, tokenized_texts = prepare_batch_for_inference(
-            sentences)
+            document)
         preds = get_predictions(ner_model, batch_for_inference)
-        write_results(tokenized_texts, preds, offset_sentence_map, out_path)
+        write_results(tokenized_texts, preds, outpath)
 
 
 def parse_args(args):
@@ -218,9 +176,10 @@ def parse_args(args):
         formatter_class=argparse.RawDescriptionHelpFormatter,
         epilog=epilog)
     parser.add_argument('--config', help='Path to the config', type=str),
-    parser.add_argument('--eval_data', help='Path to the eval_data', type=str)
     parser.add_argument(
-        '--outpath', help='Path to the results folder', type=str)
+        '--note_events', help='Path to the eval_data', type=str)
+    parser.add_argument(
+        '--outpath', help='Path to the results folder: folder/mimic_precictions.txt', type=str)
     return parser.parse_args(args)
 
 
